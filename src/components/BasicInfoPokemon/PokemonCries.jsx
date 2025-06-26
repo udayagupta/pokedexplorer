38e490de662aeea2f59b225ddb5dceb6e06ff9a3
udayagupta <udayagupta304@gmail.com>
2025-06-26 14:02:26 +0530

    refactor: optimize rendering of Pokemon cries by using Object.entries and filtering out empty URLs

--- src/components/BasicInfoPokemon/PokemonCries.jsx
+++ src/components/BasicInfoPokemon/PokemonCries.jsx
@@ -5,20 +5,22 @@ export const PokemonCries = ({ cries, className }) => {
 
   return (
     <ul className="flex gap-3 cries">
-      {Object.keys(cries).map((key, index) => (
-        <li
-          key={index}
-          className={`${className} capitalize rounded-[100vw] bg-slate-900 flex gap-4 hover:bg-slate-800 p-[5px] px-3 cursor-pointer items-center justify-center`}
-        >
-          <button
-            className="flex font-semibold h-full w-full cursor-pointer items-center gap-2 justify-center"
-            onClick={() => new Audio(cries[key]).play()}
+      {Object.entries(cries)
+        .filter(([_, url]) => url)
+        .map(([key, url], index) => (
+          <li
+            key={index}
+            className={`${className} capitalize rounded-[100vw] bg-slate-900 flex gap-4 hover:bg-slate-800 p-[5px] px-3 cursor-pointer items-center justify-center`}
           >
-            <PiSpeakerHigh />
-            <span className="text-sm capitalize">{key} cry</span>
-          </button>
-        </li>
-      ))}
+            <button
+              className="flex font-semibold h-full w-full cursor-pointer items-center gap-2 justify-center"
+              onClick={() => new Audio(url).play()}
+            >
+              <PiSpeakerHigh />
+              <span className="text-sm capitalize">{key} cry</span>
+            </button>
+          </li>
+        ))}
     </ul>
   );
 };
